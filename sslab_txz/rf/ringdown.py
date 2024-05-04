6da6a25af2f9ce6fb85b8fef0798583e75e872a3
Tony Zhang <txz@stanford.edu>
2024-05-03 18:32:47 -0700

    Create RingdownSet

--- sslab_txz/rf/ringdown.py
+++ sslab_txz/rf/ringdown.py
@@ -1,4 +1,8 @@
+# for forward references; needed until python 3.13
+from __future__ import annotations
+
 from pathlib import Path
+from typing import Mapping, Optional, Self
 
 import h5py
 import lmfit
@@ -6,13 +10,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import skrf as rf
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike, NDArray
 from scipy.constants import pi
 
 from sslab_txz.plotting import sslab_style
 
 
-class Ringdown():
+class RingdownSet():
     functional_form = (
         R'$\tilde{s}_0\, \exp(-\frac{1}{2}\kappa t-i\Delta\omega t) '
         R'+ \tilde{s}_\infty$'
@@ -22,13 +26,43 @@ class Ringdown():
     t: NDArray[np.float64]
     frequency: float
 
-    def __init__(self, h5path: str):
+    def __init__(
+            self,
+            t: ArrayLike,
+            s21: ArrayLike,
+            frequency: float,
+            temperatures: Optional[Mapping[str, float]],
+            stage_positions: Mapping[str, float],
+    ):
+        '''
+        Parameters
+        ----------
+        t: arraylike, (m,)
+            Time points in the ringdown(s)
+        s21: arraylike, (n, m) or (m,)
+        frequency: float
+        temperatures: Mapping[str, float], optional
+        stage_positions: Mapping[str, float]
+        '''
+        self.t = np.array(t)
+        self.s21 = np.array(s21).reshape(-1, len(self.t))
+
+        self.frequency = frequency
+        self.temperatures = temperatures
+        self.stage_positions = stage_positions
+
+    @classmethod
+    def from_h5(cls, h5path: str) -> Self:
+        '''
+        h5path: str
+            Path to the H5 file
+        '''
         with h5py.File(h5path, 'r') as f:
-            self.s21 = f['data/s_params/s21/real'][:] \
+            s21 = f['data/s_params/s21/real'][:] \
                 + 1j * f['data/s_params/s21/imag'][:]
-            self.t = f['data/times'][:]
-            self.frequency = f.attrs['frequency']
-            self.temperatures = {
+            t = f['data/times'][:]
+            frequency = f.attrs['frequency']
+            temperatures = {
                 key: f.attrs[key]
                 for key in [
                     'temperature_still',
@@ -38,10 +72,12 @@ class Ringdown():
                 ]
             }
 
-            self.stage_positions = {
+            stage_positions = {
                 'aaj': f.attrs['aaj_position'],
                 'aak': f.attrs['aak_position'],
             }
+
+        return cls(t, s21, frequency, temperatures, stage_positions)
 
     @staticmethod
     def ringdown_shape(
@@ -62,19 +98,29 @@ class Ringdown():
         shifted_pole = -2 * pi * (0.5 * fwhm + 1j * delta_f)
         return offset_cmplx + a0_cmplx * np.exp(shifted_pole * t)
 
+    def __getitem__(self, key: int) -> Ringdown:
+        return Ringdown(self.t, self.s21[key], self.frequency, None, self.stage_positions)
+
+    def __len__(self) -> int:
+        return len(self.s21)
+
+
+class Ringdown(RingdownSet):
     def fit_model(self, init_params=dict()):
+        s21 = self.s21[0]
+
         self.model = lmfit.Model(self.ringdown_shape)
 
-        guess_offset = self.s21[-10:].mean()
-        guess_prefactor = self.s21[0] - guess_offset
+        guess_offset = s21[-10:].mean()
+        guess_prefactor = s21[0] - guess_offset
 
         lookahead_ind = np.searchsorted(self.t, 0.1 * self.t[-1])
         lookahead_time = self.t[lookahead_ind]
-        lookahead_s21 = self.s21[lookahead_ind] - guess_offset
+        lookahead_s21 = s21[lookahead_ind] - guess_offset
         guess_fwhm = np.log(np.abs(guess_prefactor / lookahead_s21)) \
             / lookahead_time / pi
 
-        s21_scale = 0.5 * np.max(np.abs(self.s21 - self.s21.mean()))
+        s21_scale = 0.5 * np.max(np.abs(s21 - s21.mean()))
         default_init_params = dict(
             a0=dict(
                 value=np.abs(guess_prefactor),
@@ -102,7 +148,7 @@ class Ringdown():
         params = self.model.make_params(**(default_init_params | init_params))
 
         fit_range_trunc_ind = 0
-        self.s21_fit = self.s21[fit_range_trunc_ind:]
+        self.s21_fit = s21[fit_range_trunc_ind:]
         self.t_fit = self.t[fit_range_trunc_ind:]
         self.fit = self.model.fit(
             self.s21_fit,
@@ -111,6 +157,8 @@ class Ringdown():
         )
 
     def visualize(self, onering: bool = False):
+        s21 = self.s21[0]
+
         fig, axs = plt.subplot_mosaic(
             [
                 ['db', 'reim'],
@@ -124,19 +172,19 @@ class Ringdown():
 
         axs['db'].plot(
             1e+6 * self.t,
-            rf.complex_2_db(self.s21),
+            rf.complex_2_db(s21),
             label=fr'$\omega/2\pi$ = {self.frequency / 1e+9:.9f} GHz',
         )
 
         axs['deg'].plot(
             1e+6 * self.t,
-            np.unwrap(rf.complex_2_degree(self.s21), period=360),
+            np.unwrap(rf.complex_2_degree(s21), period=360),
         )
 
         reim_scale = 1e+6
         reim_scale_latex = '10^6'
         axs['reim'].plot(
-            *rf.complex_2_reim(reim_scale * self.s21),
+            *rf.complex_2_reim(reim_scale * s21),
         )
 
         if hasattr(self, 'fit') and hasattr(self.fit, 'uvars'):
