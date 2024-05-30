6d32c83bdf6ab9fc1ade23df11f8b57488515b21
Tony Zhang <txz@stanford.edu>
2024-05-30 11:15:59 -0700

    New fit models; refactored plotting

--- sslab_txz/rf/ringdown.py
+++ sslab_txz/rf/ringdown.py
@@ -2,11 +2,12 @@
 from __future__ import annotations
 
 import importlib.resources
-from typing import Iterable, Mapping, Optional
+from typing import Iterable, Literal, Mapping, Optional, cast
 
 import lmfit
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.special
 import skrf as rf
 from lmfit import Parameters
 from numpy.typing import NDArray
@@ -60,10 +61,12 @@ class RingdownSet(CWMeasurement):
     def _init_params_from_s21(
             self,
             s21: NDArray,
+            model: Literal['spiral', 'circularized'] = 'spiral',
             suffix: Optional[str] = None,
             init_params: Mapping[str, Mapping] = dict(),
     ) -> Parameters:
         guess_offset = s21[-10:].mean()
+        guess_std = np.real(s21[-10:]).std()
         guess_prefactor = s21[0] - guess_offset
 
         lookahead_ind = np.searchsorted(self.t, 0.1 * self.t[-1])
@@ -75,19 +78,14 @@ class RingdownSet(CWMeasurement):
         s21_scale = 0.5 * np.max(np.abs(s21 - s21.mean()))
 
         full_suffix = '' if suffix is None else f'_{suffix}'
-        default_init_params = dict(
+
+        common_params = dict(
             a0=dict(
                 value=np.abs(guess_prefactor),
                 min=0.5*np.abs(guess_prefactor),
                 max=2*np.abs(guess_prefactor),
             ),
-            phi0=dict(
-                value=np.angle(guess_prefactor),
-                min=-2*pi,
-                max=2*pi,
-            ),
             fwhm=dict(value=guess_fwhm, min=1, max=8e+3),
-            delta_f=dict(value=0, min=-10e+3, max=+10e+3),
             offset_re=dict(
                 value=np.real(guess_offset),
                 min=np.real(guess_offset)-s21_scale,
@@ -99,7 +97,37 @@ class RingdownSet(CWMeasurement):
                 max=np.imag(guess_offset)+s21_scale,
             ),
         )
-        full_init_params = default_init_params | init_params
+        if model == 'spiral':
+            model_specific_params = dict(
+                phi0=dict(
+                    value=np.angle(guess_prefactor),
+                    min=-2*pi,
+                    max=2*pi,
+                ),
+                delta_f=dict(value=0, min=-10e+3, max=+10e+3),
+            )
+        elif model == 'circularized':
+            model_specific_params = dict(
+                eps=dict(
+                    value=guess_std,
+                    min=0,
+                    max=10*guess_std,
+                )
+            )
+        elif model == 'abssq':
+            model_specific_params = dict(
+                a0=dict(
+                    value=np.abs(guess_prefactor)**2,
+                    min=0.5*np.abs(guess_prefactor)**2,
+                    max=2*np.abs(guess_prefactor)**2,
+                ),
+                eps=dict(
+                    value=2*guess_std**2,
+                    min=0,
+                    max=20*guess_std**2,
+                )
+            )
+        full_init_params = common_params | model_specific_params | init_params
 
         params = Parameters()
         for param_name, param_spec in full_init_params.items():
@@ -111,7 +139,7 @@ class RingdownSet(CWMeasurement):
 
     def fit_model(self, shared_params: Iterable[str] = ['fwhm', 'offset_re', 'offset_im']):
         fit_params: Parameters = sum(
-            (self._init_params_from_s21(s21i, str(i)) for i, s21i in enumerate(self.s21)),
+            (self._init_params_from_s21(s21i, suffix=str(i)) for i, s21i in enumerate(self.s21)),
             start=Parameters(),
         )
 
@@ -142,18 +170,81 @@ class RingdownSet(CWMeasurement):
 
 
 class Ringdown(RingdownSet):
-    def fit_model(self, init_params=dict()):
-        s21 = self.s21[0]
-        self.model = lmfit.Model(self.ringdown_shape)
-        params = self._init_params_from_s21(s21, init_params=init_params)
+    @staticmethod
+    def circularized_gaussian_pseudoresidual(params, t, data=None):
+        # unpack parameters: extract .value attribute for each parameter
+        parvals = params.valuesdict()
+        offset = parvals['offset_re'] + 1j * parvals['offset_im']
+        fwhm = parvals['fwhm']
+        a0 = parvals['a0']
+        eps = parvals['eps']
 
-        fit_range_trunc_ind = 0
-        self.s21_fit = s21[fit_range_trunc_ind:]
-        self.t_fit = self.t[fit_range_trunc_ind:]
-        self.fit = self.model.fit(
-            self.s21_fit,
-            params=params,
-            t=self.t_fit,
+        model_abs = a0 * np.exp(-pi * fwhm * t)
+
+        if data is None:
+            return model_abs
+
+        data_radial = np.abs(data - offset)
+        i0_arg = model_abs * data_radial / eps**2
+        i0e_val = scipy.special.i0e(i0_arg)  # I_0(x) exp(-|x|)
+        neg_log_likelihood = (
+            -np.log(data_radial) + 2 * np.log(eps) + (data_radial**2 + model_abs**2) / (2 * eps**2)
+            - (np.log(i0e_val) + np.abs(i0_arg))
+        )
+        return np.sqrt(2 * (1e+2 + neg_log_likelihood))
+
+    @staticmethod
+    def abs_residual(params, t, data=None):
+        parvals = params.valuesdict()
+        offset = parvals['offset_re'] + 1j * parvals['offset_im']
+        fwhm = parvals['fwhm']
+        a0 = parvals['a0']
+        eps = parvals['eps']
+
+        model_abs_sq = a0 * np.exp(-2 * pi * fwhm * t) + eps
+        data_radial_sq = np.abs(data - offset)**2
+        return data_radial_sq - model_abs_sq
+
+    def fit_model(self, model='spiral', init_params=dict()):
+        s21 = self.s21[0]
+        params = self._init_params_from_s21(s21, init_params=init_params, model=model)
+
+        if model == 'spiral':
+            self.model = lmfit.Model(self.ringdown_shape)
+
+            fit_range_trunc_ind = 0
+            self.s21_fit = s21[fit_range_trunc_ind:]
+            self.t_fit = self.t[fit_range_trunc_ind:]
+            self.fit = self.model.fit(
+                self.s21_fit,
+                params=params,
+                t=self.t_fit,
+            )
+        elif model == 'circularized':
+            return lmfit.minimize(
+                self.circularized_gaussian_pseudoresidual,
+                params,
+                args=(self.t,),
+                kws=dict(data=s21),
+            )
+        elif model == 'abssq':
+            return lmfit.minimize(
+                self.abs_residual,
+                params,
+                args=(self.t,),
+                kws=dict(data=s21),
+            )
+
+    def plot_cartesian(self, scale=1, ax: Optional[plt.Axes] = None, **kwargs):
+        if ax is None:
+            fig, ax = plt.subplots()
+            ax = cast(plt.Axes, ax)
+
+        ax.set_aspect(1)
+        ax.set_box_aspect(1)
+        ax.plot(
+            *rf.complex_2_reim(scale * self.s21[0]),
+            **kwargs,
         )
 
     def visualize(self, onering: bool = False):
@@ -183,9 +274,7 @@ class Ringdown(RingdownSet):
 
         reim_scale = 1e+6
         reim_scale_latex = '10^6'
-        axs['reim'].plot(
-            *rf.complex_2_reim(reim_scale * s21),
-        )
+        self.plot_cartesian(ax=axs['reim'], scale=reim_scale)
 
         if hasattr(self, 'fit') and hasattr(self.fit, 'uvars'):
             model_val = self.fit.eval(t=self.t_fit)
