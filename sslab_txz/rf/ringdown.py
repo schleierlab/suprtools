02e4f144b77e1100acaac2ca7a35e2cd990ab529
Tony Zhang <txz@stanford.edu>
2024-06-18 13:44:11 -0700

    intro'd ringdown fit classes

--- sslab_txz/rf/ringdown.py
+++ sslab_txz/rf/ringdown.py
@@ -9,8 +9,12 @@ import lmfit
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.special
+import skimage.restoration
 import skrf as rf
 from lmfit import Parameters
+from lmfit.minimizer import MinimizerResult
+from lmfit.model import ModelResult
+from lmfit.models import LinearModel
 from numpy.typing import NDArray
 from PIL import Image
 from scipy.constants import pi
@@ -183,7 +187,7 @@ class RingdownSet(CWMeasurement):
         def extract_fwhm(ringdown: Ringdown):
             fit = ringdown.fit_model(model='abssq', init_params=init_params)
             try:
-                fit = cast(lmfit.minimizer.MinimizerResult, fit)
+                fit = cast(MinimizerResult, fit)
                 return cast(UFloat, fit.uvars['fwhm'])
             except AttributeError:
                 return None
@@ -219,6 +223,11 @@ class RingdownSet(CWMeasurement):
             return np.array(residual_arr).flatten()
 
         return lmfit.minimize(multi_objective, fit_params, args=(self.t, self.s21))
+
+    def collective_fit(self) -> RingdownCollectiveFit:
+        fit = RingdownCollectiveFit(self)
+        fit.fit()
+        return fit
 
     def visualize(self, onering: bool = False):
         raise NotImplementedError
@@ -259,6 +268,12 @@ class Ringdown(RingdownSet):
         model_abs_sq = a0 * np.exp(-2 * pi * fwhm * t) + eps
         data_radial_sq = np.abs(data - offset)**2
         return data_radial_sq - model_abs_sq
+
+    def fit_scalar(self):
+        fit = RingdownScalarFit(self)
+        fit.fit()
+        fit.fit_phase()
+        return fit
 
     def fit_model(self, model='spiral', init_params=dict()):
         s21 = self.s21[0]
@@ -385,3 +400,196 @@ class Ringdown(RingdownSet):
                 ax_ring.axis('off')
 
         return fig, axs
+
+    def _repr_html_(self):
+        fig, _ = self.visualize()
+        return fig._repr_html_()
+
+
+class RingdownCollectiveFit:
+    ringdown_set: RingdownSet
+    fit_result: Optional[MinimizerResult] = None
+
+    def __init__(self, ringdown_set: RingdownSet):
+        self.ringdown_set = ringdown_set
+
+    @property
+    def t(self):
+        return self.ringdown_set.t
+
+    def residual(self, params):
+        parvals = params.valuesdict()
+        offset = parvals['offset_re'] + 1j * parvals['offset_im']
+        fwhm = parvals['fwhm']
+        a0 = parvals['a0']
+        const = parvals['const']
+
+        begin_time = 1e-6
+        begin_ind = np.searchsorted(self.t, begin_time)
+
+        model_abs_sq = a0 * np.exp(-2 * pi * fwhm * self.t[begin_ind:]) + const
+        data_radial_sq = np.abs(self.ringdown_set.s21[:, begin_ind:] - offset)**2
+        return data_radial_sq.mean(axis=0) - model_abs_sq
+
+    def fit(self):
+        max_fwhm = 0.1 / (self.t[1] - self.t[0])
+        min_fwhm = 0.1 / (self.t[-1] - self.t[0])
+
+        offset_guess = self.ringdown_set.s21[:, -20:].mean()
+        params = lmfit.create_params(
+            a0=dict(
+                value=np.mean(np.abs(self.ringdown_set.s21[:, 0] - offset_guess)**2),
+                min=0,
+            ),
+            fwhm=dict(
+                value=np.sqrt(min_fwhm * max_fwhm),
+                min=min_fwhm,
+                max=max_fwhm,
+            ),
+            offset_re=np.real(offset_guess),
+            offset_im=np.imag(offset_guess),
+            const=dict(value=self.ringdown_set.s21[:, -10:].std()**2, min=0),
+        )
+        self.fit_result = lmfit.minimize(self.residual, params)
+
+    def plot_fit(self, ax: Optional[plt.Axes] = None, xscale=1):
+        if ax is None:
+            fig, ax = plt.subplots()
+        if self.fit_result is None:
+            raise ValueError
+        if not hasattr(self.fit_result, 'uvars'):
+            raise ValueError
+
+        uvars = self.fit_result.uvars
+        offset = uvars['offset_re'].n + 1j * uvars['offset_im'].n
+        ax.plot(
+            xscale * self.t,
+            np.mean(np.abs(self.ringdown_set.s21 - offset)**2, axis=0)
+        )
+        ax.plot(
+            xscale * self.t,
+            uvars['a0'].n * np.exp(-2 * pi * uvars['fwhm'].n * self.t) + uvars['const'].n,
+            color='red',
+            label=Rf'$\kappa/2\pi = {uvars['fwhm']:SL} $ Hz',
+        )
+        ax.set_yscale('log')
+        ax.legend()
+
+    def _repr_html_(self) -> Optional[str]:
+        if self.fit_result is None:
+            return None
+        if not hasattr(self.fit_result, 'uvars'):
+            return f'Unsuccessful collective fit of {repr(self.ringdown_set)}'
+
+        fig, ax = plt.subplots()
+        self.plot_fit(ax)
+        return fig._repr_html_()
+
+
+class RingdownScalarFit(RingdownCollectiveFit):
+    ringdown_set: Ringdown
+    phase_fit: Optional[ModelResult] = None
+
+    def __init__(self, ringdown: Ringdown):
+        self.ringdown_set = ringdown
+
+    @property
+    def ringdown(self):
+        return self.ringdown_set[0]
+
+    def fit_phase(self):
+        if self.fit_result is None:
+            raise RuntimeError
+        if not hasattr(self.fit_result, 'uvars'):
+            raise RuntimeError
+
+        uvars = self.fit_result.uvars
+        offset = uvars['offset_re'].n + 1j * uvars['offset_im'].n
+
+        # phase_cyc = np.unwrap(np.angle(self.ringdown.s21[0] - offset)) / (2 * pi)
+        phase_rad = np.angle(self.ringdown.s21[0] - offset)
+        phase_cyc = skimage.restoration.unwrap_phase(phase_rad) / (2 * pi)
+
+        model = LinearModel()
+        self.phase_fit = model.fit(phase_cyc, x=self.t, offset=phase_cyc[0], slope=0)
+
+    def _repr_html_(self) -> Optional[str]:
+        if self.fit_result is None:
+            return None
+        if not hasattr(self.fit_result, 'uvars'):
+            return f'Unsuccessful collective fit of {repr(self.ringdown)}'
+        uvars = self.fit_result.uvars
+        offset = uvars['offset_re'].n + 1j * uvars['offset_im'].n
+
+        if self.phase_fit is None:
+            fig, axs = plt.subplots(
+                figsize=(6.4, 4.8),
+                nrows=2,
+                sharex=True,
+                layout='constrained',
+            )
+            ax_cart, ax_db = cast(tuple[plt.Axes, ...], axs)
+        else:
+            fig, axs = plt.subplot_mosaic(
+                [
+                    ['cart', 'db'],
+                    ['cart', 'phase'],
+                ],
+                layout='constrained',
+                figsize=(9.6, 4),
+            )
+            axs = cast(Mapping[str, plt.Axes], axs)
+            ax_cart = axs['cart']
+            ax_db = axs['db']
+            ax_phase = axs['phase']
+            ax_phase.sharex(ax_db)
+            ax_db.xaxis.set_visible(False)
+
+        self.ringdown.plot_cartesian(scale=1e+3, ax=ax_cart)
+        ax_cart.scatter(
+            1e+3 * np.real(offset),
+            1e+3 * np.imag(offset),
+            marker='+',
+            color='red',
+            zorder=2,  # = default Axes.plot zorder, s.t. this is plotted over data
+            label=(
+                R'$10^3 \times S_{21}(\infty) = '
+                fR'{1e+3 * uvars["offset_re"]:SL}'
+                fR'+ {1e+3 * uvars["offset_im"]:SL} i$'
+            ),
+        )
+        ax_cart.legend()
+
+        ax_db.plot(
+            self.t,
+            rf.complex_2_db10(np.mean(np.abs(self.ringdown.s21 - offset)**2, axis=0)),
+        )
+
+        model_vals = uvars['a0'].n * np.exp(-2 * pi * uvars['fwhm'].n * self.t) \
+            + uvars['const'].n
+        ax_db.plot(
+            self.t,
+            rf.complex_2_db10(model_vals),
+            color='red',
+            label=Rf'$\kappa/2\pi = {uvars['fwhm']:SL} $ Hz',
+        )
+        ax_db.legend()
+
+        if self.phase_fit is not None:
+            ax_phase.plot(
+                self.t,
+                self.phase_fit.data,
+            )
+            ax_phase.plot(
+                self.t,
+                self.phase_fit.best_fit,
+                color='red',
+                label=(
+                    R'$\omega - \overline{{\omega_0}} = '
+                    Rf'{-self.phase_fit.uvars['slope']:SL} \times 2\pi$ Hz'
+                ),
+            )
+            ax_phase.legend()
+
+        fig.suptitle(f'Fit to ringdown at {self.ringdown_set.frequency:,.0f} Hz')
+        return fig._repr_html_()
