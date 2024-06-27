49715f9351676cd8aa1a1b249f2ba3ef47d56644
Tony Zhang <txz@stanford.edu>
2024-06-26 19:19:14 -0700

    Insertion loss code

---
+++ sslab_txz/rf/insertion_loss.py
@@ -0,0 +1,121 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+import numpy as np
+import scipy.ndimage
+import skrf as rf
+import uncertainties
+from numpy.typing import ArrayLike
+from uncertainties import unumpy as unp
+
+
+class LossElement(ABC):
+    @abstractmethod
+    def loss_db(self, freq: ArrayLike):
+        raise NotImplementedError
+
+    def __add__(self, other: Any) -> CascadedLossElement:
+        if not isinstance(other, LossElement):
+            return NotImplemented
+        return CascadedLossElement(self, other)
+
+    def __radd__(self, other: Any) -> CascadedLossElement:
+        if not isinstance(other, LossElement):
+            return NotImplemented
+        return CascadedLossElement(other, self)
+
+
+@dataclass
+class CascadedLossElement(LossElement):
+    elt_a: LossElement
+    elt_b: LossElement
+
+    def loss_db(self, freq: ArrayLike):
+        return self.elt_a.loss_db(freq) + self.elt_b.loss_db(freq)
+
+
+class MeasuredLossyLine(LossElement):
+    measurement: rf.Network
+    uncertainty_network: rf.Network
+
+    def __init__(
+            self,
+            measurement: rf.Network,
+            measurement_b: rf.Network,
+            uncertainty_bandwidth: float = 20e+9,
+    ):
+        '''
+        measurement: Network
+            Single-port network with S21 measurement of loss element.
+        measurement_b: Network
+            Second measurement of same loss element at slightly
+            different conditions. Used to estimate uncertainty
+            (as the rms value of `measurement_b / measurement`, smoothed
+            over `uncertainty_bandwidth`)
+        uncertainty_bandwidth: float
+            Bandwidth over which to
+        '''
+        self.measurement = measurement
+        uncertainty_estimator = measurement_b / measurement
+
+        df = measurement.frequency.df[0]
+        filter_points = int(uncertainty_bandwidth // df)
+
+        # running rms of [network ratio in dB]
+        uncertainty_estimate_db = np.sqrt(scipy.ndimage.uniform_filter1d(
+            rf.complex_2_db(uncertainty_estimator.s[:, 0, 0])**2,
+            size=filter_points,
+        ))
+
+        self.uncertainty_network = rf.Network(
+            f=self.measurement.f,
+            s=rf.db_2_mag(uncertainty_estimate_db),
+        )
+
+    @staticmethod
+    def _interpolate_network(network: rf.Network, freq: ArrayLike):
+        freq = np.asarray(freq)
+        interpolated_net = network.interpolate(np.atleast_1d(freq), coords='polar')
+        return interpolated_net.s[:, 0, 0].reshape(freq.shape)
+
+    def uncertainty_db(self, freq):
+        return rf.complex_2_db(
+            self._interpolate_network(self.uncertainty_network, freq),
+        )
+
+    def loss_db(self, freq: ArrayLike):
+        freq_arr = np.asarray(freq)
+
+        freq_flat = freq_arr.flatten()
+        nominal_db_flat = rf.complex_2_db(self._interpolate_network(self.measurement, freq_flat))
+        uncert_db_flat = self.uncertainty_db(freq_flat)
+
+        uvars_flat = np.array(uncertainties.correlated_values(
+            nominal_db_flat,
+            np.diag(uncert_db_flat ** 2),
+        ))
+
+        if freq_arr.size == 1 and not isinstance(freq, np.ndarray):
+            return uvars_flat.item()
+
+        return uvars_flat.reshape(freq_arr.shape)
+
+
+@dataclass
+class RootFrequencyLossElement(LossElement):
+    '''
+    loss_constant: float
+        Loss constant in units of dB / sqrt(Hz) such that insertion loss
+        becomes loss_constant * sqrt(frequency)
+    '''
+    loss_constant: float
+
+    def loss_db(self, freq: ArrayLike):
+        return self.loss_constant * unp.sqrt(freq)
+
+
+# 1 mm f/f ("bullet")
+# 6/17 dB @ 50 GHz, 9/17 dB @ 110 GHz
---
+++ tests/test_loss_elements.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pytest
+import skrf as rf
+
+from sslab_txz.rf.insertion_loss import (MeasuredLossyLine,
+                                         RootFrequencyLossElement)
+
+
+@pytest.fixture
+def root_f_elt():
+    return RootFrequencyLossElement(-1)
+
+
+@pytest.fixture
+def measured_elt():
+    '''
+    Measured element with 20+/-1 dB insertion loss
+    '''
+    f = np.arange(70, 115, 5) * 1e+9
+    s = np.full_like(f, 0.1)
+    net = rf.Network(f=f, s=s)
+    net_b = rf.Network(f=f, s=(s * 10**(1/20)))
+
+    return MeasuredLossyLine(net, net_b)
+
+
+class TestRootFrequencyLossElement:
+    '''
+    Partitions for input loss constants:
+        type (float, ufloat)
+
+    Testing partitions for loss_db:
+
+    freq:
+        type (scalar, nestedlists, or ndarray)
+        ndim (0, 1)
+        shape along axis (0, 1, many)
+        value (Hz, kHz, MHz, GHz ranges)
+    '''
+    def test_scalar(self, root_f_elt: RootFrequencyLossElement):
+        assert root_f_elt.loss_db(25) == -5
+
+    def test_ndim_0(self, root_f_elt: RootFrequencyLossElement):
+        assert root_f_elt.loss_db(np.array(36)) == -6
+
+    def test_ndim_1_single(self, root_f_elt: RootFrequencyLossElement):
+        assert root_f_elt.loss_db([4]) == -2
+
+    def test_ndim_1(self, root_f_elt: RootFrequencyLossElement):
+        np.testing.assert_array_equal(
+            root_f_elt.loss_db([1e+4, 1e+6, 1e+8, 1e+10]),
+            [-1e+2, -1e+3, -1e+4, -1e+5],
+        )
+
+    def test_empty_ndim_1(self, root_f_elt: RootFrequencyLossElement):
+        np.testing.assert_array_equal(
+            root_f_elt.loss_db([]),
+            [],
+        )
+
+    def test_ndim_2(self, root_f_elt: RootFrequencyLossElement):
+        np.testing.assert_array_equal(
+            root_f_elt.loss_db([[144., 196.], [0.81, 0.64]]),
+            [[-12., -14.], [-0.9, -0.8]],
+        )
+
+    def test_return_ndarray(self, root_f_elt: RootFrequencyLossElement):
+        assert isinstance(root_f_elt.loss_db([1, 4]), np.ndarray)
+
+
+class TestMeasuredLossyLine:
+    def test_rms_error(self, measured_elt: MeasuredLossyLine):
+        np.testing.assert_almost_equal(
+            measured_elt.loss_db(100e+9).s,
+            1,
+        )
+
+
+class TestCascadedElement:
+    def test_addition(self, measured_elt: MeasuredLossyLine):
+        root_f_elt_ghz = RootFrequencyLossElement(-1 / np.sqrt(100e+9))
+        sum_elt = root_f_elt_ghz + measured_elt
+        assert sum_elt.loss_db(100e+9).n == -(1 + 20)
+
+    def test_multiple_addition(self, measured_elt: MeasuredLossyLine):
+        root_f_elt_ghz = RootFrequencyLossElement(-1 / np.sqrt(100e+9))
+        sum_elt = root_f_elt_ghz + measured_elt + root_f_elt_ghz
+        assert sum_elt.loss_db(100e+9).n == -(2 + 20)
