6b53b7bbaaf04b09beaf7132d78020713231a370
Tony Zhang <txz@stanford.edu>
2024-09-09 22:27:11 -0700

    Add import to sslab_txz.rf

--- sslab_txz/rf/__init__.py
+++ sslab_txz/rf/__init__.py
@@ -1,6 +1,7 @@
 from .cw import CWMeasurement  # noqa
 from .errors import FitFailureError  # noqa
 from .freqdomain import (ScanDataFilter, VectorFittingFancy,  # noqa
-                         WideScanData, WideScanNetwork, fit_mode, test_a_fit)
+                         WideScanData, WideScanNetwork, fit_mode, fit_network,
+                         test_a_fit)
 from .modedata import *  # noqa
 from .ringdown import Ringdown, RingdownSet  # noqa
