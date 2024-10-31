946f985225238f3c65bfee506a7a1f7b99ba475e
Tony Zhang <txz@stanford.edu>
2024-10-30 20:54:38 -0700

    extended plotting package with paper plot settings

---
+++ sslab_txz/plotting/style.py
@@ -0,0 +1,59 @@
+from numbers import Real
+
+import numpy as np
+import seaborn as sns
+from scipy.constants import pi
+from uncertainties import unumpy as unp
+
+textwidth_pts = 510
+columnwidth_pts = 246
+columnwidth_mpl_in = 5  # dimension for for plt.subplots()
+textwidth_mpl_in = columnwidth_mpl_in * textwidth_pts / columnwidth_pts
+
+polarization_markers = ('.', 'x')
+mode_palette = sns.cubehelix_palette(
+    n_colors=15,
+    start=0,
+    rot=6,
+    reverse=True,
+    gamma=0.8,
+    dark=0.2,
+    light=0.7,
+)
+
+
+def kwarg_func_factory(
+        label: bool | str,
+        markers: tuple[str, str] = polarization_markers,
+        palette=mode_palette,
+        **kwargs,
+):
+    def kwarg_func(mode_data, q, pol):
+        polstr = 'x' if pol == +1 else 'y'
+
+        base_label = fr'$\mathrm{{TEM}}_{{{q},0,0,{polstr}}}$'
+        if mode_data is not None:
+            if isinstance(mode_data, Real):
+                meanfreq = mode_data
+            else:
+                freqs = unp.nominal_values(mode_data['pole_i']) / (2 * pi)
+                meanfreq = np.nanmean(freqs)
+
+            base_label = fr'$\mathrm{{TEM}}_{{{q},0,0,{polstr}}}$ @ {meanfreq/1e+9:.4f} GHz'
+
+        match label:
+            case bool(b):
+                label_str = base_label if b else ''
+            case str(suffix):
+                label_str = ' '.join([base_label, suffix])
+            case _:
+                raise TypeError
+
+        final_kwargs = dict(
+            alpha=0.5,
+            color=palette[q-20],
+            marker=(markers[0] if pol == +1 else markers[1]),
+            label=label_str,
+        ) | kwargs
+        return final_kwargs
+    return kwarg_func
