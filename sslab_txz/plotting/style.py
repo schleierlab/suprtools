487f2109b0d5ff27bd07c83a1acca760571f5e48
Tony Zhang <txz@stanford.edu>
2024-11-05 19:48:47 -0800

    extend mode color palette for mode sets with != 15 modes

--- sslab_txz/plotting/style.py
+++ sslab_txz/plotting/style.py
@@ -11,25 +11,30 @@ columnwidth_mpl_in = 5  # dimension for for plt.subplots()
 textwidth_mpl_in = columnwidth_mpl_in * textwidth_pts / columnwidth_pts
 
 polarization_markers = ('.', 'x')
-mode_palette = sns.cubehelix_palette(
-    n_colors=15,
-    start=0,
-    rot=6,
-    reverse=True,
-    gamma=0.8,
-    dark=0.2,
-    light=0.7,
-)
+
+
+def mode_palette(n_colors):
+    return sns.cubehelix_palette(
+        n_colors=n_colors,
+        start=0,
+        rot=6,
+        reverse=True,
+        gamma=0.8,
+        dark=0.2,
+        light=0.7,
+    )
 
 
 def kwarg_func_factory(
         label: bool | str,
+        q_range: tuple[int, int],
         markers: tuple[str, str] = polarization_markers,
-        palette=mode_palette,
+        palette_func=mode_palette,
         **kwargs,
 ):
     def kwarg_func(mode_data, q, pol):
         polstr = 'x' if pol == +1 else 'y'
+        palette = palette_func(q_range[1] - q_range[0] + 1)
 
         base_label = fr'$\mathrm{{TEM}}_{{{q},0,0,{polstr}}}$'
         if mode_data is not None:
@@ -51,7 +56,7 @@ def kwarg_func_factory(
 
         final_kwargs = dict(
             alpha=0.5,
-            color=palette[q-20],
+            color=palette[q - q_range[0]],
             marker=(markers[0] if pol == +1 else markers[1]),
             label=label_str,
         ) | kwargs
