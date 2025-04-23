4656f80888178d0a012abb2837de7d7d0ea971eb
Tony Zhang <txz@stanford.edu>
2025-04-22 18:17:39 -0700

    stanford theme colors

---
+++ sslab_txz/plotting/colors.py
@@ -0,0 +1,13 @@
+from types import SimpleNamespace
+
+stanford = SimpleNamespace(
+    cardinal_red_dark = '#820000',
+    cardinal_red_light = '#B83A4B',
+    palo_alto_green = '#175E54',
+    palo_alto_green_light = '#2D716F',
+    spirited_dark = '#C74632',
+    plum_light = '#734675',
+    plum = '#620059',
+    archway_light = '#766253',
+    archway = '#5D4B3C',
+)
