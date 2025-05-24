f68c13f7de7966f16b12e1c98b49712c10413157
Tony Zhang <txz@stanford.edu>
2025-05-23 17:44:21 -0700

    use siunitx in labels

--- sslab_txz/zygo.py
+++ sslab_txz/zygo.py
@@ -16,6 +16,9 @@ from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 from numpy.typing import NDArray
 from uncertainties import unumpy as unp
 
+from sslab_txz.plotting import mpl_usetex
+from sslab_txz.plotting.units import Units
+
 
 # TODO: maybe deprecate in favor of `prysm` package?
 class ZygoProfile:
@@ -95,7 +98,7 @@ class QuadraticFit:
 
         ax.set_aspect('equal')
         cbar = fig.colorbar(im, ax=ax)
-        cbar.set_label(R'$z$ ($\mu$m)')
+        cbar.set_label(Rf'$z$ ({Units.UM.mplstr()})')
         ax.set_title('Raw data')
 
     def plot_summary(self):
@@ -119,8 +122,8 @@ class QuadraticFit:
         axs[1, 1].set_title('Uncertainty correlations')
 
         for ax in axs.ravel()[:3]:
-            ax.set_xlabel(R'$x$ ($\mu$m)')
-            ax.set_ylabel(R'$y$ ($\mu$m)')
+            ax.set_xlabel(Rf'$x$ ({Units.UM.mplstr()})')
+            ax.set_ylabel(Rf'$y$ ({Units.UM.mplstr()})')
 
     def curvature(self):
         axx, axy, ayy = self.upopt[3:]
@@ -192,7 +195,11 @@ class QuadraticFit:
             asb = AnchoredSizeBar(
                 ax.transData,
                 scale_val,
-                Rf'{scale_val} $\mu$m',
+                (
+                    Rf'\SI{{{scale_val}}}{{\micro\meter}}'
+                    if mpl_usetex() else
+                    Rf'{scale_val} $\mu$m'
+                ),
                 loc=8,
                 pad=0.1,
                 borderpad=1,
@@ -205,13 +212,13 @@ class QuadraticFit:
 
         ra = np.abs(quadratic_residuals_nm).mean()
         rq = quadratic_residuals_nm.std()
-        roughness_str = f'Ra, Rq = {ra:.1f}, {rq:.1f} nm'
+        roughness_str = f'Ra, Rq = {ra:.1f}, {rq:.1f} {Units.NM.mplstr()}'
         title_lines = [roughness_str]
         if is_ax_fresh:
             cbar = fig.colorbar(im, ax=ax)
-            cbar.set_label('$z - z^{(2)}$ (nm)')
-            ax.set_xlabel(R'$x$ ($\mu$m)')
-            ax.set_ylabel(R'$y$ ($\mu$m)')
+            cbar.set_label(f'$z - z^{{(2)}}$ ({Units.NM.mplstr()})')
+            ax.set_xlabel(Rf'$x$ ({Units.UM.mplstr()})')
+            ax.set_ylabel(Rf'$y$ ({Units.UM.mplstr()})')
             title_lines = ['Quadratic fit residuals', roughness_str]
         ax.set_title('\n'.join(title_lines))
 
@@ -237,10 +244,10 @@ class QuadraticFit:
         im = ax.pcolormesh(x_um, y_um, self.mirror_data_window - self.linear_fit_result(x_um, y_um))
         cbar = fig.colorbar(im, ax=ax)
         ax.set_title('Linear fit residuals')
-        cbar.set_label('$z - z^{(1)}$ (nm)')
+        cbar.set_label(f'$z - z^{(1)}$ ({Units.NM.mplstr()})')
 
-        ax.set_xlabel(R'$x$ ($\mu$m)')
-        ax.set_ylabel(R'$y$ ($\mu$m)')
+        ax.set_xlabel(Rf'$x$ ({Units.UM.mplstr()})')
+        ax.set_ylabel(Rf'$y$ ({Units.UM.mplstr()})')
 
     def linear_fit_result(self, x, y):
         a0, ax, ay = self.popt[0:3]
